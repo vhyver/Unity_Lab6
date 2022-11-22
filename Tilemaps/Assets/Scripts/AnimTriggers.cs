60925e5dcbc01702d0a98cc4a757acf13347cb47
Vadym Verba <71729845+vhyver@users.noreply.github.com>
2022-11-22 20:52:20 +0200

    2D

---
+++ Tilemaps/Assets/Scripts/AnimTriggers.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTriggers : MonoBehaviour
+{
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void ResetAnimation()
+    {
+        anim.SetBool("isRun", false);
+        anim.SetBool("isJump", false);
+    }
+    public void Idle()
+    {
+        ResetAnimation();
+        anim.SetTrigger("idle");
+    }
+
+    public void Run()
+    {
+        ResetAnimation();
+        anim.SetBool("isRun", true);
+
+    }
+    public void Jump()
+    {
+        ResetAnimation();
+        anim.SetBool("isJump", true);
+
+    }
+}
---
+++ Tilemaps/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movePower = 10f;
+    public float jumpPower = 15f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    Vector3 movement;
+    private int direction = 1;
+    bool isJumping = false;
+
+    //[SerializeField] private TrailRenderer tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Jump();
+        Run();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        anim.SetBool("isJump", false);
+    }
+
+
+    void Run()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+        anim.SetBool("isRun", false);
+
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            direction = -1;
+            moveVelocity = Vector3.left;
+
+            transform.localScale = new Vector3(direction, 1, 1);
+            if (!anim.GetBool("isJump"))
+                anim.SetBool("isRun", true);
+
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            direction = 1;
+            moveVelocity = Vector3.right;
+
+            transform.localScale = new Vector3(direction, 1, 1);
+            if (!anim.GetBool("isJump"))
+                anim.SetBool("isRun", true);
+
+        }
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+    void Jump()
+    {
+        if ((Input.GetButtonDown("Jump") || Input.GetAxisRaw("Vertical") > 0))
+        {
+            isJumping = true;
+            anim.SetBool("isJump", true);
+        }
+        if (!isJumping)
+        {
+            return;
+        }
+
+        rb.velocity = Vector2.zero;
+
+        Vector2 jumpVelocity = new Vector2(0, jumpPower);
+        rb.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+        isJumping = false;
+    }
+}
